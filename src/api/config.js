4e063b7370186c3b703f9503c5a45595379e9672
Victor Manuel Murillo Camayo <mcvictor@unicauca.edu.co>
2019-04-12 02:22:23 -0500

    Fixes por la url

--- src/api/config.js
+++ src/api/config.js
@@ -5,6 +5,6 @@ export default{
     UrlImage: 'http://image.tmdb.org/t/p/original/',
     UrlClasificacionR: 'https://api.themoviedb.org/3/discover/movie?api_key=d2f75c50a366b48f468d9a270511e992&certification_country=US&certification=R&sort_by=vote_average.desc',
     UrlNinos: 'http://api.themoviedb.org/3/discover/movie?api_key=d2f75c50a366b48f468d9a270511e992&certification_country=US&certification_country=US&certification.lte=G&sort_by=popularity.desc',
-    UrlDrama: 'https://api.themoviedb.org/3/discover/movie?api_key=d2f75c50a366b48f468d9a270511e992&with_genres=18&primary_release_year=2019',
-    UrlSearchMovie: 'https://api.themoviedb.org/3/search/movie?api_key=d2f75c50a366b48f468d9a270511e992&query='
+    UrlDrama: 'http://api.themoviedb.org/3/discover/movie?api_key=d2f75c50a366b48f468d9a270511e992&with_genres=18&primary_release_year=2019',
+    UrlSearchMovie: 'http://api.themoviedb.org/3/search/movie?api_key=d2f75c50a366b48f468d9a270511e992&query='
 }
